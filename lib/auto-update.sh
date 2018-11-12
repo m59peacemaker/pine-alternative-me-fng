f38e67aa36d8e3e20b82f2fac0c580fb6cd97680
Johnny Hauser <johnnyhauser@gmail.com>
2018-11-11 23:03:16 -0600

    "ci skip" prevents triggering build via API call (or any other way), skipping via custom commit message instead

--- lib/auto-update.sh
+++ lib/auto-update.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+build_commit_message="[build]"
+
+if [ "${TRAVIS_COMMIT_MESSAGE}" = "${build_commit_message}" ]; then
+	echo "This job was triggered by the build commit. Skipping build."
+	exit 0
+fi
+
 npm install --no-save https://github.com/m59peacemaker/data-alternative-me-fng
 
 npm run build
@@ -20,7 +27,7 @@ if [ -n "`git status dist -s`" ]; then
 		echo 'committing build to git...'
 		git checkout master
 		git add dist
-		git commit -m "FnG build `date --iso-8601=minutes` [ci skip]"
+		git commit -m "${build_commit_message}"
 		git remote remove origin
 		git remote add origin "https://${GITHUB_TOKEN}@github.com/m59peacemaker/pine-alternative-me-fng.git" > /dev/null 2>&1
 		git push --quiet origin master
